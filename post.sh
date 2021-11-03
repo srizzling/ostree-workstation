cd3afe70c210649db809e2f9454d158452f888b6
Sriram Venkatesh <venksriram@gmail.com>
2021-11-03 00:01:07 -0400

    wip

--- post.sh
+++ post.sh
@@ -1,7 +1,18 @@
-#/bin/sh
+#!/usr/bin/env bash
+set -xeuo pipefail
 
-# Boot to console
-# disable getty and reset default to graphical.target 
-# if using display manager
-systemctl set-default multi-user.target
-systemctl enable getty@tty1.service
+
+# NetworkManager config
+cat <<EOF > /usr/lib/NetworkManager/conf.d/local.conf
+[main]
+plugins=
+[device]
+#wifi.backend=iwd
+EOF
+
+# systemd resolved
+ln -sfn /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+ln -s ../systemd-resolved.service /usr/lib/systemd/system/multi-user.target.wants/systemd-resolved.service
+
+mkdir -p /usr/lib/systemd/system/getty.target.wants
+ln -s ../getty@.service /usr/lib/systemd/system/getty.target.wants/getty@tty1.service
