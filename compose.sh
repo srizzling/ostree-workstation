77e395b55f01bf160b7a03a3d46edeb87b7b0d18
Sriram Venkatesh <venksriram@gmail.com>
2021-11-04 08:14:56 -0400

    tidy up

--- compose.sh
+++ compose.sh
@@ -9,4 +9,4 @@ if [ ! -d $REPO/objects ]; then
     ostree --repo=$REPO init --mode=archive-z2
 fi
 
-rpm-ostree compose tree --unified-core --cachedir=$CACHE --repo=$REPO fedora-qtile.yml
+rpm-ostree compose tree --unified-core --cachedir=$CACHE --repo=$REPO fedora-srizzling.yml
